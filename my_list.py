f5e53885619adb58dd006bf6093affe981f2cd5a
sheu-1 <86465368+sheu-1@users.noreply.github.com>
2024-11-15 21:17:30 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,21 @@
+my_list = []
+list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# my_list[2] = 15
+
+list.append(50)
+list.append(60)
+list.append(70)
+
+
+# my_list.append(list)
+
+my_list.remove(40)
+
+my_list.sort()
+
+print(my_list[2])
